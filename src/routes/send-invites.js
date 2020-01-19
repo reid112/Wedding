ae00502fa41cf56bae8a42acbf0c105daa568891
Reid <rreid112@gmail.com>
2020-01-19 08:47:19 -0600

    Refactored email logic

--- src/routes/send-invites.js
+++ src/routes/send-invites.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sendEmail = require('../utils/send-email');
+const { createInviteEmail } = require('../utils/create-email');
 const { checkAuthenticated } = require('../utils/auth-helper');
 const { Invite } = require('../models');
 const router = express.Router();
@@ -10,16 +11,7 @@ router.post("/", checkAuthenticated, async (req, res) => {
     const invites = await Invite.find({});
 
     invites.forEach(function (invite) {
-      const url = process.env.HOST + '/invite/' + invite.guid
-
-      const message = {
-          from: 'rsvp@brittaniandriley.com',
-          to: invite.email,
-          subject: 'Wedding Invite - Brittani and Riley!',
-          html: invite.names +",<br/><br/>You are invited to our wedding! <a href='" + url + "'>View your invite</a> for the wedding details and use the form on the website to RSVP.<br/><br/> If you have any questions, feel free to reply to this email! <br/><br/>We hope to see you there!<br/>Brittani and Riley"
-      };
-
-      sendEmail(message);
+      sendEmail(createInviteEmail(invite));
     });
 
     res.json({success : "Success", status : 200});
--- src/utils/create-email.js
+++ src/utils/create-email.js
@@ -1,14 +1,36 @@
-const createRsvpEmail = ({ isNewRsvp, name, email, rsvpBool, numberAttending, names, notes }) => {
+const createRsvpEmail = ({ isNewRsvp, name, email, attending, numberAttending, names, notes }) => {
+    const emailText = `
+        Name: ${name}<br/>
+        Email: ${email}<br/>
+        Attending: ${attending}<br/>
+        Number: ${numberAttending}<br/>
+        Names: ${names}<br/>Notes: ${notes}
+    `;
+
     return Object.freeze({
-        from: 'rsvp@brittaniandriley.com',
+        from: '"RSVP" <rsvp@brittaniandriley.com>',
         to: 'brittaniandriley@gmail.com',
         subject: (isNewRsvp) ? 'New RSVP!' : 'Updated RSVP!',
-        text: 'Name: ' + name + '\nEmail: ' + email + '\nAttending: ' + rsvpBool + '\nNumber: ' + numberAttending + '\nNames: ' + names + '\nNotes: ' + notes
+        html: emailText
     });
 };
 
-const createInviteEmail = () => {
-    // TODO:
+const createInviteEmail = ({ guid, email, names }) => {
+    const url = `${process.env.HOST}/invite/${guid}`
+    const emailText = `
+        ${names},<br/><br/>
+        You are invited to our wedding! <a href='${url}'>View your invite</a> for the wedding details and use the 
+        form on the website to RSVP.<br/><br/> If you have any questions, feel free to reply to this email! <br/><br/>
+        We hope to see you there!<br/>
+        Brittani and Riley
+    `;
+
+    return Object.freeze({
+        from: '"Brittani and Riley" <brittaniandriley@gmail.com>',
+        to: email,
+        subject: 'Wedding Invite - Brittani and Riley!',
+        html: emailText
+    });
 };
 
 module.exports = {
